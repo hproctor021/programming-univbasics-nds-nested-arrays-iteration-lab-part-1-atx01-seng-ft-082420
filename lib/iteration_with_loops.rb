f74df142b55afe68543bf3f2b07b7885477fc582
Haley Proctor <hproctor021@gmail.com>
2020-08-04 19:14:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_even_values(array_of_arrays)
  while row_index < array_of_arrays.count do
    element_index = 0
    while element_index < array_of_arrays[row_index].count do
-     p array_of_arrays[row_index][element_index]
+     p array_of_arrays[row_index].even?
      element_index += 1
    end
    row_index += 1
